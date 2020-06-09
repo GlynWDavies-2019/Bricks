aedea820ef594129eeac5277f87dc514b8217bf0
Glyn Davies <glynwdavies@gmail.com>
2020-06-09 17:04:23 +0100

    Game logic completed

--- script.js
+++ script.js
@@ -77,6 +77,8 @@ const brickInfo = {
     visible: true
 };
 
+// Build a matrix of bricks
+
 const bricks = [];
 for(let i = 0 ; i < brickRowCount; i++) {
     bricks[i] = [];
@@ -101,13 +103,105 @@ function drawBricks() {
     });
 }
 
+// Move paddle
+
+function movePaddle() {
+    paddle.x += paddle.dx; 
+    if(paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+// Move Ball
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    // Wall collision x
+    if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        ball.dx *= -1;
+    }
+    // Wall collision y
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+    // Paddle collision
+    if(ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed;
+    }
+    // Brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if(brick.visible) {
+                if (
+                    ball.x - ball.size > brick.x &&
+                    ball.x + ball.size < brick.x + brick.w &&
+                    ball.y + ball.size > brick.y &&
+                    ball.y - ball.size < brick.y + brick.h
+                  ) {
+                    ball.dy *= -1;
+                    brick.visible = false;
+                    increaseScore();
+                  }
+            }
+        })
+    })
+    if(ball.y + ball.size > canvas.height) {
+        showAllBricks();
+        score = 0;
+    }
+}
+
+function increaseScore() {
+    score++;
+    if(score % (brickRowCount * brickRowCount) === 0) {
+        showAllBricks();
+    }
+}
+
+function showAllBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => brick.visible = true)
+    })
+}
+
 // Draw everything
 
 function draw() {
+    context.clearRect(0,0,canvas.width, canvas.height);
     drawBall();
     drawPaddle();
     drawScore();
     drawBricks();
 }
 
-draw();
+function update() {
+    movePaddle();
+    moveBall();
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
+
+function keyDown(event) {
+   if(event.key === 'ArrowRight' || event.key === 'Right') {
+       paddle.dx = paddle.speed;
+   } else if(event.key === 'ArrowLeft' || event.key === 'Left') {
+       paddle.dx = -paddle.speed;
+   }
+}
+
+function keyUp(event) {
+    if(event.key === 'ArrowRight' || event.key === 'Right' || event.key === 'ArrowLeft' || event.key === 'Left') {
+        paddle.dx = 0;
+    }
+}
+
+// Keyboard Events
+
+document.addEventListener('keydown',keyDown);
+document.addEventListener('keyup',keyUp);
